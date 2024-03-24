19663a402c4455b531f7912c2b570e0aad0fbcff
Shre12333 <shreyashbele@gmail.com>
2024-03-25 00:21:09 +0530

    Perform addition using two variables

--- Source.java
+++ Source.java
@@ -5,6 +5,9 @@ public class Source
     public static void main(String[] args) 
     {
         int a = 23;
+        int b = 24;
         System.out.println("The value of a is " + a);
+        System.out.println("The value of a is " + a);
+        System.out.println("The addition of a and b is " + (a + b));
     }
 }
