62fe166c7a48521676edbd3a84807f19a47972d3
Shre12333 <shreyashbele@gmail.com>
2024-03-25 00:18:51 +0530

    Add java file

---
+++ Source.java
@@ -0,0 +1,10 @@
+package Local_Repo;
+
+public class Source
+{
+    public static void main(String[] args) 
+    {
+        int a = 23;
+        System.out.println("The value of a is " + a);
+    }
+}
